f12e338e494e27b4648d2e8fda0ed024c1803b9b
Farshad <f.bagdeli13@gmail.com>
2024-05-15 15:19:22 +0200

    base css

---
+++ src/templates/api.js
@@ -0,0 +1,42 @@
+// https://boolean-uk-api-server.fly.dev/fbagdeli/post
+const post = {
+  "id": 1,
+  "contactId": 4,
+  "title": "Ventosus cimentarius suus pecco.",
+  "content": "Vereor sapiente verus curvo attero spargo suadeo caelestis uter atque. Utroque suscipio ciminatio cursim tamdiu sodalitas ex. Pecco antepono vespillo facere umquam terebro taceo. Tondeo autem adhuc velum valens apto pauci. Comminor defessus certus taedium sto. Vado brevis cito conventus quibusdam sollers statim autem."
+}
+
+
+// https://boolean-uk-api-server.fly.dev/fbagdeli/contact
+const contact ={
+  firstName: "Laurel",
+  lastName: "Zboncak",
+  gender: "Hermaphrodite",
+  email: "Shawn31@hotmail.com",
+  jobTitle: "National Identity Orchestrator",
+  street: "Anne Point",
+  city: "Kutchburgh",
+  latitude: 56.2147,
+  longitude: -42.9807,
+  favouriteColour: "#2eae37",
+  profileImage: "https://www.gravatar.com/avatar/Shawn31@hotmail.com?s=120&d=identicon",
+  id: 1
+}
+//https://boolean-uk-api-server.fly.dev/fbagdeli/post/3/comment
+const comment = {
+  "id": 13,
+  "postId": 3,
+  "contactId": 8,
+  "content": "Tantillus debeo cado circumvenio appello excepturi crustulum campana suadeo copia."
+}
+
+
+
+// Header background, button & font colour: #000046
+// Form input background colour: #e6ebf5
+// Button hover colour: #64dc78
+// Post title colour: #64648c
+// Body background colour: #F0F5FA
+// Header logo SVG
+// Home icon SVG
+// Profile icon SVG

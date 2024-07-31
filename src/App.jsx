265c77ee210add9592f195f46afb002530b278ca
Farshad <f.bagdeli13@gmail.com>
2024-07-31 11:43:57 +0200

    modified main

--- src/App.jsx
+++ src/App.jsx
@@ -202,9 +202,7 @@ function App() {
                     
                   </ul>
                   <div className='comments-input'>
-                    <svg width="33" height="36" viewBox="0 0 33 36"   fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.5 36V12L16.6 0L32.5 12V36H20.8V21.75H12.15V36H0.5Z" fill="#64648C" />
-                    </svg>
+                  <button id='userInitials'>{getInitials(contactPost)}</button>
                     <div>
                       <input type='text' placeholder='Add a comment...'></input>
                       
